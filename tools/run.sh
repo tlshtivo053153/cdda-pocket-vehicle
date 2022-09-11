bd2917a17a71fd8acf2f2c522aa6740c5f77afe5
tlshtivo053153 <muq8inubwlxpcv4oemcazmpfwowkrco-git@yahoo.co.jp>
2022-09-11 23:42:23 +0900

    Add mod for magiclysm

--- tools/run.sh
+++ tools/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_filter_not_abstract.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_filter_type_vehicle_part.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_foldable_to_1ml.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_unfoldable_to_250ml.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_extend_flags_foldable.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/jq_copyfrom.sh {}
-find ./pocket_vehicle/vehicleparts/ -name '*.json' | xargs -I {} ./tools/grep_not_null.sh {}
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_filter_not_abstract.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_filter_type_vehicle_part.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_foldable_to_1ml.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_unfoldable_to_250ml.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_extend_flags_foldable.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_copyfrom.sh" {}
+find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/grep_not_null.sh" {}
