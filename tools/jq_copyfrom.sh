9450fae441000dcb939a0db38714e00423eaa66c
tlshtivo053153 <muq8inubwlxpcv4oemcazmpfwowkrco-git@yahoo.co.jp>
2022-09-16 14:05:49 +0900

    folding_time and unfolding_time to one second

--- tools/jq_copyfrom.sh
+++ tools/jq_copyfrom.sh
@@ -2,6 +2,6 @@
 
 FILENAME="$1"
 
-JQ_COMMAND='map(. | { "copy-from": .id, id: .id, type: .type, name: .name, looks_like: .looks_like, "folded_volume": .folded_volume, extend: .extend})'
+JQ_COMMAND='map(. | { "copy-from": .id, id: .id, type: .type, name: .name, looks_like: .looks_like, "folded_volume": .folded_volume, "folding_time": "1 seconds", "unfolding_time": "1 seconds", extend: .extend})'
 
 cat "$FILENAME" | jq "$JQ_COMMAND" | json_formatter.cgi | sponge "$FILENAME"
