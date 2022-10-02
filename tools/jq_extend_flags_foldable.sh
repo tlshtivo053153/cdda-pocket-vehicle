c5a10255393d12a83bd62b6e0c988e9f2aebca91
tlshtivo053153 <muq8inubwlxpcv4oemcazmpfwowkrco-git@yahoo.co.jp>
2022-10-02 16:15:55 +0900

    Remove FOLDABLE flag

--- tools/jq_extend_flags_foldable.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-FILENAME="$1"
-
-JQ_COMMAND='map(. | .+ { "extend": { "flags": [ "FOLDABLE" ] } })'
-cat "$FILENAME" | jq "$JQ_COMMAND" | json_formatter.cgi | sponge "$FILENAME"
--- tools/run.sh
+++ tools/run.sh
@@ -6,6 +6,5 @@ find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_f
 find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_filter_type_vehicle_part.sh" {}
 find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_foldable_to_1ml.sh" {}
 find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_unfoldable_to_250ml.sh" {}
-find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_extend_flags_foldable.sh" {}
 find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/jq_copyfrom.sh" {}
 find ./ -not -name 'modinfo.json' -name '*.json' | xargs -I {} "$SCRIPT_DIR/grep_not_null.sh" {}
